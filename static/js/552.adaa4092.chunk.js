4d202fa61faa3bfb386d124a650583b9f3ba01d7
chry902 <chry902@users.noreply.github.com>
2022-11-18 22:15:57 +0000

    deploy: 5855b425bfa6267358a3ded16f2670ad2f10f9a4

---
+++ static/js/552.adaa4092.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpadel_stat=self.webpackChunkpadel_stat||[]).push([[552],{552:function(e,l,n){n.r(l),n.d(l,{default:function(){return t}});var r="styles_home_container__PywIb",s=n(87),a=n(184),t=function(){return(0,a.jsx)("div",{className:r,children:(0,a.jsxs)("ul",{children:[(0,a.jsx)("li",{children:(0,a.jsx)(s.rU,{to:"/AddPlayer",children:"New Player"})}),(0,a.jsx)("li",{children:(0,a.jsx)(s.rU,{to:"/Challenge",children:"Inizia allenamento"})})]})})}}}]);
+//# sourceMappingURL=552.adaa4092.chunk.js.map
